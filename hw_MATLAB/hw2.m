6fc7c9e9df96b30f29ad432acdf6cec64003bd3c
Bo Zhang <journeyonthegoing@gmail.com>
2018-02-24 16:44:17 -0600

    Update for print intersection

--- hw_MATLAB/hw2.m
+++ hw_MATLAB/hw2.m
@@ -7,53 +7,39 @@ info = h5info(fileName, location);
 lines = numel(info.Groups) - 1;
 cells = struct2cell(info.Groups);
 names = cells(1, :);
-positions = numel(h5read(fileName, strcat(names{lines}, '/calls')));
+cols = numel(h5read(fileName, strcat(names{lines}, '/calls')));
 % some names appear to be stored in another cell if they are too long
 for i = 1:lines
     if (~isempty(cells{2, i}))
         names{i} = cells{2, i}.Name;
     end
 end
-data = zeros(lines, positions);
+data = zeros(lines, cols);
 for idx = 1:lines
     data(idx, :) = (h5read(fileName, strcat(names{idx}, '/calls')))';
 end
 %% N = -1, A = 0, T = 51, C = 17, G = 34
-intersections = zeros(1, positions);
-for i = 1:positions
+intersections = zeros(1, cols);
+for i = 1:cols
     for j = 1:lines
         if (data(j, i) ~= -1) 
             intersections(i) = intersections(i) + 1;
         end
     end
 end
-%% the set of genomic positions having two or more SNPs (“intersecting SNPs”)
+%% the set of genomic positions having two or more SNPs (â€œintersecting SNPsâ€)
 sites = find(intersections ~= 0);
 %% the names of the lines and their non-N variants at each position 
-%% currently this part takes too long to calculate
-% nonNs = cell(1, positions);
-% for i = 1:positions
-%     nonNs{i} = strings(1, intersections(i));
-%     count = 1;
-%     for j = 1:lines
-%         if (data(j, i) ~= -1) 
-%             nonNs{i}(count) = erase(names(j), strcat(location, '/'));
-%             count = count + 1;
-%         end
-%     end
-% end
-%% print intersections
+%% print intersections, file size > 1G
 % fileID = fopen('intersections.txt','w');
-% output = strings(lines, positions);
-% strings(:, :) = '-';
-% for i = 1:positions
-%     count = 1;
+% for i = 1:cols
+%     fprintf(fileID,'%d\r',i);
 %     for j = 1:lines
 %         if (data(j, i) ~= -1) 
-%             output(count, i) = erase(names(i), strcat(location, '/'));
-%             count = count + 1;
+%             fprintf(fileID,'%s\r',erase(names{j}, strcat(location, '/')));
 %         end
 %     end
+%     fprintf(fileID,'\n');
 % end
 % fclose(fileID);
 %% the number of lines at each intersecting SNP and their distribution of intersecting SNP positions across the genome 
